1d6cf399b917806691b86736931ae004728c64f5
Nikita <neymarenko123@gmail.com>
2020-12-02 19:12:15 +0200

    add group service to the group action

--- src/reducers/userReducer/actions.ts
+++ src/reducers/userReducer/actions.ts
@@ -2,22 +2,26 @@ import {Dispatch} from 'redux';
 import {ActionTypes} from "./actionTypes";
 import {StateType} from "../../store";
 import {TelegramResponse} from "./reducer";
-const {SET_GROUP, SET_USER} = ActionTypes;
 
-const setGroup = (group:string) => async (dispatch:Dispatch, getState:()=>StateType) => {
-    dispatch({
-        type: SET_GROUP,
-        payload: group
-    })
-}
+const {SET_GROUP, SET_USER} = ActionTypes;
+import GroupService from "../../api/services/groupService";
+
+const setGroup = (group: string) =>
+    async (dispatch: Dispatch, getState: () => StateType) => {
+        const groupService = new GroupService();
+        groupService.saveGroup(group);
+        dispatch({
+            type: SET_GROUP,
+            payload: group
+        })
+    }
 // todo add redux-thunk action and change User type if that would be need
-const setUser = (user : TelegramResponse) => async (dispatch:Dispatch) => {
+const setUser = (user: TelegramResponse) => async (dispatch: Dispatch) => {
     dispatch({
         type: SET_USER,
         payload: user
     })
 }
-
 
 
 export {
